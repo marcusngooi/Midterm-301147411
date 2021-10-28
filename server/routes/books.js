bc0b7cdf405b484650653f8f347105e5bf5660d3
Marcus Ngooi <mngooi@gmail.com>
2021-10-28 16:08:54 -0400

    Completed draft of BooksList page implementation and started Books Routing File implementation

--- server/routes/books.js
+++ server/routes/books.js
@@ -1,70 +1,93 @@
 // modules required for routing
-let express = require('express');
+let express = require("express");
 let router = express.Router();
-let mongoose = require('mongoose');
+let mongoose = require("mongoose");
 
 // define the book model
-let book = require('../models/books');
+let book = require("../models/books");
 
 /* GET books List page. READ */
-router.get('/', (req, res, next) => {
+router.get("/", (req, res, next) => {
   // find all books in the books collection
-  book.find( (err, books) => {
+  book.find((err, books) => {
     if (err) {
       return console.error(err);
-    }
-    else {
-      res.render('books/index', {
-        title: 'Books',
-        books: books
+    } else {
+      res.render("books/index", {
+        title: "Books",
+        books: books,
       });
     }
   });
-
 });
 
 //  GET the Book Details page in order to add a new Book
-router.get('/add', (req, res, next) => {
-
-    /*****************
-     * ADD CODE HERE *
-     *****************/
-
+router.get("/add", (req, res, next) => {
+  /*****************
+   * ADD CODE HERE *
+   *****************/
+  res.render("books/details", {
+    title: "Add Book",
+  });
 });
 
 // POST process the Book Details page and create a new Book - CREATE
-router.post('/add', (req, res, next) => {
+router.post("/add", (req, res, next) => {
+  /*****************
+   * ADD CODE HERE *
+   *****************/
+  let newBook = Book({
+    Title: req.body.Title,
+    Description: req.body.Description,
+    Price: req.body.Price,
+    Author: req.body.Author,
+    Genre: req.body.Genre,
+  });
 
-    /*****************
-     * ADD CODE HERE *
-     *****************/
-
+  Book.create(newBook, (err, Contact) => {
+    if (err) {
+      console.log(err);
+      res.end(err);
+    } else {
+      // refresh the favourite books list
+      res.redirect("/books-index");
+    }
+  });
 });
 
 // GET the Book Details page in order to edit an existing Book
-router.get('/:id', (req, res, next) => {
+router.get("/:id", (req, res, next) => {
+  /*****************
+   * ADD CODE HERE *
+   *****************/
+  let id = req.params.id;
 
-    /*****************
-     * ADD CODE HERE *
-     *****************/
+  Book.findById(id, (err, bookToUpdate) => {
+    if (err) {
+      console.log(err);
+      res.end(err);
+    } else {
+      // show the edit view
+      res.render("book/details", {
+        title: "Update Book",
+        book: bookToUpdate,
+      });
+    }
+  });
 });
 
 // POST - process the information passed from the details form and update the document
-router.post('/:id', (req, res, next) => {
-
-    /*****************
-     * ADD CODE HERE *
-     *****************/
-
+router.post("/:id", (req, res, next) => {
+  /*****************
+   * ADD CODE HERE *
+   *****************/
 });
 
 // GET - process the delete by user id
-router.get('/delete/:id', (req, res, next) => {
-
-    /*****************
-     * ADD CODE HERE *
-     *****************/
+router.get("/delete/:id", (req, res, next) => {
+  /*****************
+   * ADD CODE HERE *
+   *****************/
 });
-
 
 module.exports = router;
